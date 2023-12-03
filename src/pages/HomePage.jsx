ad15772ee03ec5ea695cd5aece14ef40d940b820
Eugene-ak <amedioreugene@gmail.com>
2023-12-03 22:42:48 +0000

    Corrected errors in App.js

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 
-const Homepage = () => {
+const HomePage = () => {
     return (
         <>
             <Header />
@@ -9,4 +9,4 @@ const Homepage = () => {
     )
 }
 
-export default Homepage
+export default HomePage;
