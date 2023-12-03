4b0bfe10b79e7fb54c4e37db4a698fe416049453
Eugene-ak <amedioreugene@gmail.com>
2023-12-03 22:28:19 +0000

    Completed nav bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Logo from '../constants/images/Logo';
+
+const Header = () => {
+    return (
+        <>
+            <nav>
+                <div className='logo'>
+                    <Logo />
+                </div>
+                <div className='nav-list'>
+                    <ul>
+                        <li><a href='./pages/HomePage'>Home</a></li>
+                        <li><a href='./pages/BlogPage'>Blog</a></li>
+                        <li><a href='./pages/SinglePostsPage'>Single Post</a></li>
+                        <li><a href='./pages/PostsPage'>Posts</a></li>
+                        <li><a href='./pages/contactPage'>Contact</a></li>
+                    </ul>
+                </div>
+                <div className='nav-search'>
+                    <input className='search-bar' type='text' name='search' placeholder='Search' />
+                    <button className='color-mode-btn'><button></button></button>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header;
