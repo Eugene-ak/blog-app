4d6c36ba4e6e61e71999a0fad7259d93974ca315
Eugene-ak <amedioreugene@gmail.com>
2024-01-04 01:58:45 +0000

    Tweaks

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import '../stylesheets/notFound.styles.css'
 const NotFoundPage = () => {
   return (
     <main className="not-found-main">
-      <h1>404:(</h1>
+      <h1>404 :( 😥</h1>
       <h2>Page Not Found</h2>
       <p>The requested resource cannot be found</p>
     </main>
